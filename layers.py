84cac671ca7ed3a1f929f61cdbb0c4276ad98782
Neo <limingyao128@gmail.com>
2018-06-12 17:28:00 +0900

    mex

--- layers.py
+++ layers.py
@@ -3,12 +3,32 @@ from torch import nn
 from torch.nn.functional import Im2Col , Col2Im
 import pdb
 class Mex(nn.Module):
-    def __init__(self,  bias=True):
+    def __init__(self, kernel_size, 
+    stride=1, padding=0, dilation=1, groups=1,beta=3):
         super(Mex, self).__init__()
-        if bias:
-            pass
-        else:
-            self.register_parameter('bias', None)
+        helper = lambda x: x if isinstance(x, (list, tuple)) else (x, x)
+        self.kernel_size = helper(kernel_size)
+        self.stride = helper(stride)
+        self.padding = helper(padding)
+        self.dilation = helper(dilation)
+        self.groups = groups
+        self.beta = nn.Parameter(torch.Tensor(1).fill_(beta))
+    def forward(self, x):
+        N, C, H, W = x.shape
+        HH, WW = self.kernel_size[0], self.kernel_size[1]
+        H_out = 1 + (H + 2 * self.padding[0] - self.dilation[0]*(HH - 1) - 1 ) / self.stride[0] # formula from pytorch doc
+        W_out = 1 + (W + 2 * self.padding[1] - self.dilation[1]*(WW - 1) - 1) / self.stride[1]
+        x_max = x.max()
+        x = x.sub(x_max).mul(self.beta)
+        x = Im2Col.apply(x, self.kernel_size, self.dilation, self.padding, self.stride)
+        x = x.view(N, C, HH*WW, H_out*W_out)
+        x = x.mean(2)
+        # x.shape = N, C,  H_out*W_out
+        x = x.log().div(self.beta) + x_max
+        return x.view(N,C, H_out, W_out)
+
+
+
         
 
 class Similarity(nn.Conv2d):
@@ -24,34 +44,49 @@ class Similarity(nn.Conv2d):
     self.groups = groups
     '''
     def __init__(self, in_channels, out_channels, kernel_size, 
-    stride=1, padding=0, dilation=1, groups=1, sim_type='linear'):
+    stride=1, padding=0, dilation=1, groups=1, bias=True, sim_type='linear'):
+        '''
+        sim_type = linear/conv/l1/l2
+        '''
         super(Similarity, self).__init__(in_channels, out_channels, kernel_size, 
-        stride, padding, dilation, groups, bias=False) # note that is will have bias para but none forever
+        stride, padding, dilation, groups, bias=bias) # note that is will have bias para but none forever
         self.sim_type = sim_type
-        self.tamplate = nn.Parameter(torch.Tensor(self.weight.size()))
+        self.tamplate = nn.Parameter(torch.Tensor(self.weight.size())) if sim_type!='conv' else None
 
     def forward(self,x):
         N, C, H, W = x.shape
         F, C, HH, WW = self.weight.shape
-        H_out = 1 + (H + 2 * self.padding[0] - self.dilation[0]*(HH - 1) - 1 ) / self.stride[0]
+        H_out = 1 + (H + 2 * self.padding[0] - self.dilation[0]*(HH - 1) - 1 ) / self.stride[0] # formula from pytorch doc
         W_out = 1 + (W + 2 * self.padding[1] - self.dilation[1]*(WW - 1) - 1) / self.stride[1]
         x = Im2Col.apply(x, self.kernel_size, self.dilation, self.padding, self.stride)
         w = self.weight.view(1, F, C*HH*WW, 1) # w.shape = 1, F, C*HH*WW, 1
         x = x.unsqueeze(1) # x.shape = B, 1, C*HH*WW, H_out*W_out
-        x = w.mul(x).sum(-2) # outshape = B, F, H_out*W_out 
-        x = x.view(N,F,H_out, W_out)
-        if self.sim_type == 'linear':
-            pass
+        if self.sim_type == 'conv':
+            x = w.mul(x).sum(-2) # outshape = B, F, H_out*W_out 
+        elif self.sim_type == 'linear':
+            t = self.tamplate.view_as(w)
+            x = w.mul(t).mul(x).sum(-2)
         elif self.sim_type == 'l1':
-            pass
+            t = self.tamplate.view_as(w)
+            x = x.sub(t).abs().mul(w).sum(-2)
         elif self.sim_type == 'l2':
-            pass
+            t = self.tamplate.view_as(w)
+            x = x.sub(t).pow(2).mul(w).sum(-2)
+        else:
+            raise Exception("unknow sim_type: {}".format(self.sim_type)) 
+        if self.bias:
+            x = x + self.bias.view(1, F, 1)
+        x = x.view(N,F,H_out, W_out)
         return x
 
 if __name__ == '__main__':
-    sim = Similarity(64,4, (3,3), 2,1)
-    conv = torch.nn.Conv2d(64,4,(3,3),2,1, bias=False)
+    sim = Similarity(3, 4, (3,3), 2,1, bias = None,sim_type='l1')
+    mex = Mex(2,2)
+    conv = torch.nn.Conv2d(3, 4,(3,3),2,1, bias=False)
     conv.weight = sim.weight
-    x = torch.rand(2,64,300,300)
+    x = torch.rand(2,3,300,300).mul(1e-4)
     print x.shape
-    print (sim(x) - conv(x)).sum()
+    x = sim(x)
+    print x.shape
+    x = mex(x)
+    print x.shape
