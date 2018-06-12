d5ef5e2212f416f343aace9ad8d8c4358dfdd4d6
Neo <limingyao128@gmail.com>
2018-06-12 23:52:35 +0900

    fix utils

--- utils.py
+++ utils.py
@@ -18,16 +18,25 @@ class Replace(object):
                 continue
             if isinstance(attob, nn.Conv2d):
                 sim = Similarity(attob.in_channels, attob.out_channels,attob.kernel_size,
-                        attob.stride, attob.padding, attob.dilation, attob.groups, attob.bias, 
+                        attob.stride, attob.padding, attob.dilation, attob.groups, attob.bias is not None, 
                         sim_type=self.sim_type)
                 net.__setattr__(att, sim)
             elif isinstance(attob, nn.Sequential):
-                for m in attob:
-                    self.conv2sim(m)
+                ms = attob.children()
+                newms=[]
+                for m in ms:
+                    if isinstance(m, nn.Conv2d):
+                        sim = Similarity(m.in_channels, m.out_channels, m.kernel_size,
+                                m.stride, m.padding, m.dilation, m.groups, m.bias is not None, 
+                                sim_type=self.sim_type)
+                        newms.append(sim)
+                    else:
+                        self.conv2sim(m)
+                        newms.append(m)
+                net.__setattr__(att, nn.Sequential(*newms))
             elif isinstance(attob, nn.Module):
                 self.conv2sim( attob)
-        
-        pass
+
     def pool2mex(self, net):
         atts = dir(net)
         for att in atts:
@@ -39,8 +48,16 @@ class Replace(object):
                 mex = Mex(attob.kernel_size, attob.stride, attob.padding, attob.dilation)
                 net.__setattr__(att, mex)
             elif isinstance(attob, nn.Sequential):
-                for m in attob:
-                    self.pool2mex(m)
+                ms = attob.children()
+                newms=[]
+                for m in ms:
+                    if isinstance(m, nn.MaxPool2d):
+                        mex = Mex(m.kernel_size, m.stride, m.padding, m.dilation)
+                        newms.append(mex)
+                    else:
+                        self.pool2mex(m)
+                        newms.append(m)
+                net.__setattr__(att, nn.Sequential(*newms))
             elif isinstance(attob, nn.Module):
                 self.pool2mex(attob)
 
